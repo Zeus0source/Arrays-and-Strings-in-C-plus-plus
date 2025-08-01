aa5ee7a966d61b0776db54edbfbfd1b07478bfb6
Yash Rastogi <rastogiyash2006@gmail.com>
2025-08-01 10:34:41 +0530

    Add files via upload

---
+++ Arrays In C++.cpp
@@ -0,0 +1,25 @@
+//Yash Rastogi Exp-7
+//24070123135
+#include<iostream>
+using namespace std;
+int main(){
+    int num[10];
+    cout <<"Enter any 5 integer values: ";
+    for(int i=0;i<5;i++){
+        cin>>num[i];
+    }
+    for(int j=0;j<5;j++)
+    {
+        cout<<num[j]<<" ";
+    }
+}
+/*
+Output:
+Enter any 5 integer values: 50
+60
+70
+80
+90
+
+50 60 70 80 90 
+*/
---
+++ Reverse Array.cpp
@@ -0,0 +1,29 @@
+//Yash Rastogi Exp-7
+//24070123135
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter the size of array: ";
+    cin>>x;
+    int num[x];
+    cout <<"Enter any "<<x<<" integer values: ";
+    for(int i=0;i<x;i++){
+        cin>>num[i];
+    }
+    cout<<"Reversed Array is: ";
+    for(int j=x-1;j>=0;j--)
+    {
+        cout<<num[j]<<" ";
+    }
+}
+/*
+Output:
+Enter the size of array: 5
+Enter any 5 integer values: 30
+40
+50
+60
+70
+Reversed Array is: 70 60 50 40 30
+*/
